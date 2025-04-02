e898b538c418c177b585bc2d15103f0342499060
EmreUte <emreute@gmail.com>
2025-04-02 14:44:43 +0300

    Added asset folder and incorporated proper colors from figma to ./lib/utils/colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryText = Color(0xFFF5F5F5);
+  static const Color secondaryText = Colors.black;
+  static const Color fillText = Color(0x4D000000);
+  static const Color fillBox = Color(0x5ED9D9D9);
+  static const Color buttonBackground = Color(0xFF55628F);
+  static const Color appBarBackground = Color(0xFF82558F);
+}
