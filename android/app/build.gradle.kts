68be05bf3e90dae9282a2e3ebd3efd5930be5531
EmreUte <emreute@gmail.com>
2025-04-22 12:13:00 +0300

    sync to firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,26 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+// The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+dependencies {
+// Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+
+// TODO: Add the dependencies for Firebase products you want to use
+// When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+// Firebase Authentication
+    implementation("com.google.firebase:firebase-auth")
+// Cloud Firestore
+    implementation("com.google.firebase:firebase-firestore")
+
+// Add the dependencies for any other desired Firebase products
+// https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
@@ -24,7 +42,7 @@ android {
         applicationId = "com.example.cs310_project"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,8 +50,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+// TODO: Add your own signing config for the release build.
+// Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
